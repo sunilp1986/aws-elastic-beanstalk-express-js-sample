166bfa9b157a765995de0c320df67874f1aaaafa
sunilp1986 <SKUP@topsoe.dk>
2021-03-02 20:34:55 +0530

    Second committ

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello to first devop practical by Sunil'));
+app.get('/', (req, res) => res.send('Second change'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
